439b9bb71d7d096fee745720f9fefc16f0cd3a65
Collin Town <collintown@gmail.com>
2022-10-30 21:38:29 -0400

    Default log level - Updated README

--- src/helpers/logging.ts
+++ src/helpers/logging.ts
@@ -6,6 +6,8 @@ const textBlue = '\x1b[34m';
 const textCyan = '\x1b[36m';
 const textMagenta = '\x1b[35m';
 
+const logLevel = process.env.LOG_LEVEL?.toLowerCase() || 'info';
+
 enum LogLevel {
     Error = 'error',
     Warn = 'warn',
@@ -24,19 +26,19 @@ enum LogLevel {
  */
 export const logger = {
     info: (message: string) => {
-        if (process.env.LOG_LEVEL === LogLevel.Info || process.env.LOG_LEVEL === LogLevel.Debug)
+        if (logLevel === LogLevel.Info || logLevel === LogLevel.Debug)
             console.log(`${textBlue}🔵 INFO${reset}: ${message}`);
     },
     debug: (message: string) => {
-        if (process.env.LOG_LEVEL === LogLevel.Debug)
+        if (logLevel === LogLevel.Debug)
             console.log(`${textCyan}🐛 DEBUG${reset}: ${message}`);
     },
     waiting: (message: string) => {
-        if (process.env.LOG_LEVEL === LogLevel.Info || process.env.LOG_LEVEL === LogLevel.Debug)
+        if (logLevel === LogLevel.Info || logLevel === LogLevel.Debug)
             console.log(`${textMagenta}⏳ WAITING${reset}: ${message}`);
     },
     warn: (message: string) => {
-        if (process.env.LOG_LEVEL === LogLevel.Info || process.env.LOG_LEVEL === LogLevel.Debug || process.env.LOG_LEVEL === LogLevel.Warn)
+        if (logLevel === LogLevel.Info || logLevel === LogLevel.Debug || logLevel === LogLevel.Warn)
             console.log(`${textYellow}🟡 WARN${reset}: ${message}`);
     },
     success: (message: string) => {
